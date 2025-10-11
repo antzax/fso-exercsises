9fda9816b75e8f7f9676c2f7c01da0054af8ded6
anton <166389859+antzax@users.noreply.github.com>
2025-10-11 11:29:06 +0300

    Create function for favorite blog and test that

--- part4/blogilista/backend/tests/blog.test.js
+++ part4/blogilista/backend/tests/blog.test.js
@@ -1,6 +1,6 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const { dummy, totalLikes } = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const { dummy, totalLikes, favoriteBlog } = require("../utils/list_helper");
 
 const blogs = [
   {
@@ -9,7 +9,7 @@ const blogs = [
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422aa71b54a676234d17f8",
@@ -17,7 +17,7 @@ const blogs = [
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b3a1b54a676234d17f9",
@@ -25,7 +25,7 @@ const blogs = [
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b891b54a676234d17fa",
@@ -33,7 +33,7 @@ const blogs = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422ba71b54a676234d17fb",
@@ -41,7 +41,7 @@ const blogs = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422bc61b54a676234d17fc",
@@ -49,27 +49,41 @@ const blogs = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-    __v: 0
-  }  
-]
+    __v: 0,
+  },
+];
 
-test('dummy returns one', () => {
-    const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-    const result = dummy(blogs)
-    assert.strictEqual(result, 1)
-})
+  const result = dummy(blogs);
+  assert.strictEqual(result, 1);
+});
 
-describe('total likes', () => {
-    test('of empty list is zero', () => {
-        assert.strictEqual(totalLikes([]), 0)
-    })
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    assert.strictEqual(totalLikes([]), 0);
+  });
 
-    test('of one blog is same as the blog', () => {
-        assert.strictEqual(totalLikes([blogs[0]]), 7)
-    })
+  test("of one blog is that", () => {
+    assert.strictEqual(totalLikes([blogs[0]]), 7);
+  });
 
-    test('of many blogs is equal to their sum', () => {
-        assert.strictEqual(totalLikes(blogs), 36)
-    })
-})
+  test("of many blogs is equal to their sum", () => {
+    assert.strictEqual(totalLikes(blogs), 36);
+  });
+});
+
+describe("most liked", () => {
+  test("of emply list is none", () => {
+    assert.deepStrictEqual(favoriteBlog(), null);
+  });
+
+  test("of one blog is that", () => {
+    assert.deepStrictEqual(favoriteBlog([blogs[0]]), blogs[0]);
+  });
+
+  test("of many blogs is the most liked", () => {
+    assert.deepStrictEqual(favoriteBlog(blogs), blogs[2]);
+  });
+});
--- part4/blogilista/backend/utils/list_helper.js
+++ part4/blogilista/backend/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (!blogs) return null
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+
+  return blogs.find(blog => blog.likes === maxLikes)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 };
